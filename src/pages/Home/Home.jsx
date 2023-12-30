f31f6365c19fc4f25dba1bac4575b44dc6510b9b
Md Parvej <mdparvejme24@gmail.com>
2023-12-30 21:28:58 +0600

    added folder structure and install react helme

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Home = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>SmartTech | Home</title>
+            </Helmet>
+            <h2>Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
